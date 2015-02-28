a666464eb980f902de74a3520fbbb4a950c2465e
johnny manziel <johnny manziel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-28 18:23:34 +0000

    initial commit
    git-svn-id: https://plugins.svn.wordpress.org/easy-digital-shop/trunk@1101982 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-easy-digital-shop-deactivator.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @class       Easy_Digital_Shop_Deactivator
+ * @version	1.0.0
+ * @package	min-max-quantities-for-woocommerce
+ * @category	Class
+ * @author      johnny-manziel <jmkaila@gmail.com>
+ */
+
+class Easy_Digital_Shop_Deactivator {
+
+	/**
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
